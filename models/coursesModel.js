737b98258ca8294bf6f548372ca9bcce6c475119
mahi25soni <msdraj02@protonmail.com>
2024-04-08 10:14:04 +0530

    CoursesChaptersContentDone

--- models/coursesModel.js
+++ models/coursesModel.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose")
 
 const coursesSchema = new mongoose.Schema({
-    author : {
+    instructor : {
         type : mongoose.Schema.Types.ObjectId,
         ref : "Users"
     },
@@ -23,12 +23,23 @@ const coursesSchema = new mongoose.Schema({
     description : {
         type : String
     },
+    netDuration : {
+        type : Number // Joh videos daali jaayegi, unko total sum time.
+    },
     students : [
         {
             type : mongoose.Schema.Types.ObjectId,
             ref : "Users"
         }
     ],
+    chapters : [
+        {
+            type : mongoose.Schema.Types.ObjectId,
+            ref : "Chapters"
+        }
+    ]
+    // Yaha enrollment wala bhi banana hai
+    // and rating reviews wala bhi.
 },
 {
     timestamps : true
@@ -37,23 +48,48 @@ const coursesSchema = new mongoose.Schema({
 const Courses  = new mongoose.model("Courses", coursesSchema)
 
 
-const courseReviewRatingSchema = new mongoose.Schema({
+
+const chaptersSchema = new mongoose.Schema({
     course_id : {
         type : mongoose.Schema.Types.ObjectId,
         ref : "Courses"
     },
-    user_id : {
-        type : mongoose.Schema.Types.ObjectId,
-        ref : "Users"
-    },
-    reviews : [{type : String}],
-    rating : {
-        type : Number,
-        min : 0,
-        mix : 5,
+    name : {
+        type : String,
         required : true
     },
+    contentList : [
+        {
+            tupe 
+        }
+    ]
 },
 {
     timestamps : true
 })
+const Chapters = new mongoose.model("Chapters", chaptersSchema)
+
+
+const contentSchema = new mongoose.Schema({
+    chapter_id : {
+        type : mongoose.Schema.Types.ObjectId,
+        ref : "Chapters"
+    },
+    sub_topic_name : String,
+    video_url : {
+        type : String // baad mei isko required karna hai mujhe
+    },
+    study_material_url : String // ye optional hi rakhunga, better rahega bc
+},
+{
+    timestamps : true
+})
+const Content = new mongoose.model("Content", contentSchema)
+
+module.exports = {
+    Courses,
+    Chapters,
+    Content
+}
+
+
